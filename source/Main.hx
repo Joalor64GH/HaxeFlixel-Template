48c92a122399763fa8958d390a72c53cf1eebfb7
Joalor64 <rhgarcon@gmail.com>
2022-11-17 09:31:32 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -9,7 +9,7 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
-	var skipSplash:Bool = false;
+	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets'
 	
 	// You can pretty much ignore everything from here on - your code should go in your states.
