4b4b10f8279d97cd04b6867bd7bcf76167fae8d2
GuineaPigUuhh <93527295+GuineaPigUuhh@users.noreply.github.com>
2024-03-29 11:21:00 -0300

    cool Changes!

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,17 +5,17 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-    override public function create()
-    {
-        super.create();
+	override public function create()
+	{
+		super.create();
 
-        var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
-        text.screenCenter();
-        add(text);
-    }
+		var text = new FlxText(0, 0, 0, "Hello World", 64);
+		text.screenCenter();
+		add(text);
+	}
 
-    override public function update(elapsed:Float)
-    {
-        super.update(elapsed);
-    }
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
 }
